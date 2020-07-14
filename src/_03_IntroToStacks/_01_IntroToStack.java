3e4f5b34648ecbc5adc39873cf14180c07e345b1
JakeMaster32 <46147844+JakeMaster32@users.noreply.github.com>
2020-07-13 18:28:21 -0700

    Intro to stack, text undo IP

--- src/_03_IntroToStacks/_01_IntroToStack.java
+++ src/_03_IntroToStacks/_01_IntroToStack.java
@@ -9,14 +9,41 @@ public class _01_IntroToStack {
 	public static void main(String[] args) {
 		//1. Create a Stack of Doubles
 		//   Don't forget to import the Stack class
-		
+		Stack <Double> doubles = new Stack<Double>();
 		//2. Use a loop to push 100 random doubles between 0 and 100 to the Stack.
-		
+		for (int x = 0; x< 100; x++) {
+			Random rand = new Random();
+			doubles.push(rand.nextDouble()*100);
+		}
 		//3. Ask the user to enter in two numbers between 0 and 100, inclusive. 
+		boolean c = false;
+		String y = JOptionPane.showInputDialog("Enter a number between 0 and 100");
+		int y1 = Integer.parseInt(y);
+		String z = JOptionPane.showInputDialog("Enter a different number between 0 and 100");
+		int z1 = Integer.parseInt(z);
+		if (y1>z1) {
+			c = true;
+		}
+		else {
+			c = false;
+		}
 		
 		//4. Pop all the elements off of the Stack. Every time a double is popped that is
 		//   between the two numbers entered by the user, print it to the screen.
-		
+		for (int a = 0; a<100; a++) {
+			double b = doubles.pop();
+		    if (c == true) {
+		    	if (b>z1 && b<y1) {
+		    		System.out.println(b);
+		    	}
+		    }
+		    else {
+		    	if (b>y1 && b<z1) {
+		    		System.out.println(b);
+		    	}
+		    }
+		}
+	
 		
 		//   EXAMPLE:
 		//   NUM 1: 65
--- src/_03_IntroToStacks/_02_TextUndoRedo.java
+++ src/_03_IntroToStacks/_02_TextUndoRedo.java
@@ -1,18 +1,71 @@
 package _03_IntroToStacks;
 
-public class _02_TextUndoRedo {
-	/* 
+import java.awt.Component;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.util.Stack;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class _02_TextUndoRedo implements KeyListener {
+	/*
 	 * Create a JFrame with a JPanel and a JLabel.
 	 * 
-	 * Every time a key is pressed, add that character to the JLabel. It should look like a basic text editor.
+	 * Every time a key is pressed, add that character to the JLabel. It should look
+	 * like a basic text editor.
 	 * 
-	 * Make it so that every time the BACKSPACE key is pressed, the last character is erased from the JLabel.
-	 * Save that deleted character onto a Stack of Characters.
+	 * Make it so that every time the BACKSPACE key is pressed, the last character
+	 * is erased from the JLabel. Save that deleted character onto a Stack of
+	 * Characters.
 	 * 
-	 * Choose a key to be the Undo key. Make it so that when that key is pressed, the top Character is popped 
-	 * off the Stack and added back to the JLabel.
+	 * Choose a key to be the Undo key. Make it so that when that key is pressed,
+	 * the top Character is popped off the Stack and added back to the JLabel.
 	 * 
-	 * */
-	
-	
+	 */
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JLabel label = new JLabel();
+	Stack<Character> deleted = new Stack<Character>();
+
+	public static void main(String[] args) {
+		_02_TextUndoRedo object = new _02_TextUndoRedo();
+		object.setup();
+	}
+
+	void setup() {
+		frame.add(panel);
+		panel.add(label);
+		label.setText("");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+		frame.setSize(1000, 1000);
+		frame.addKeyListener(this);
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (!(e.getKeyChar() == KeyEvent.VK_DELETE) || !(e.getKeyChar() == KeyEvent.VK_LEFT)) {
+
+			label.setText(label.getText() + e.getKeyChar());
+
+		} else if (e.getKeyChar() == KeyEvent.VK_DELETE) {
+
+			deleted.push(label.getText().charAt(label.getText().length() - 1));
+
+			label.setText(label.getText().substring(0, label.getText().length() - 2));
+			System.out.println(label.getText().substring(0, label.getText().length() - 2));
+		}
+		else if (e.getKeyChar()== KeyEvent.VK_LEFT){
+			label.setText(label.getText()+deleted.pop());
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+	}
 }
