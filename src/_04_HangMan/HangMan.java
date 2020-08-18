efb8f44e8ba4a05dc11c90035ee9e63c636bfebc
JakeMaster32 <46147844+JakeMaster32@users.noreply.github.com>
2020-08-17 18:25:10 -0700

    hang man (IP)

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -10,12 +10,17 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class HangMan implements KeyListener{
+public class HangMan implements KeyListener {
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel(new BorderLayout());
 	JLabel label = new JLabel();
 	JLabel lives = new JLabel();
 	String popped = "";
+	int LIVES = 6;
+	int counter = 0;
+	int counter2 = 0;
+	int size;
+	
 
 	void setup() {
 		String y = JOptionPane
@@ -36,8 +41,7 @@ public class HangMan implements KeyListener{
 		}
 
 		popped = stack.pop();
-		
-		int LIVES = 6;
+
 		frame.add(panel);
 		panel.add(label, BorderLayout.NORTH);
 		panel.add(lives, BorderLayout.SOUTH);
@@ -45,15 +49,16 @@ public class HangMan implements KeyListener{
 		frame.setVisible(true);
 		frame.setSize(300, 300);
 		int length = popped.length();
-		String popped2 = "               ";
+		String popped2 = "";
 		for (int a = 0; a < length; a++) {
 			popped2 = popped2 + "_";
 		}
 		System.out.println(popped);
 		System.out.println(popped2);
 		label.setText(popped2);
-		lives.setText("                            LIVES: "+LIVES);
+		lives.setText("                            LIVES: " + LIVES);
 		frame.addKeyListener(this);
+		size = stack.size();
 		
 	}
 
@@ -62,46 +67,68 @@ public class HangMan implements KeyListener{
 	public static void main(String[] args) {
 		HangMan man = new HangMan();
 		man.setup();
-	
 
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stuff
 		char character = e.getKeyChar();
-		for (int b = 0; b<popped.length(); b++) {
+		for (int b = 0; b < popped.length(); b++) {
 			if (popped.charAt(b) == character) {
 				if (b == 0) {
 					String newString = popped.charAt(0) + label.getText().substring(1);
 					label.setText(newString);
-				}
-				else if (b == (popped.length()-1)) {
-					String newString = label.getText().substring(0, popped.length()-1) + popped.charAt(popped.length()-1);
+				} else if (b == (popped.length() - 1)) {
+					String newString = label.getText().substring(0, popped.length() - 1)
+							+ popped.charAt(popped.length() - 1);
 					label.setText(newString);
 					System.out.println(newString);
-				}
-				else { 
-					String newString = label.getText().substring(0,b) + popped.charAt(b) + label.getText().substring(b+1);
+				} else {
+					String newString = label.getText().substring(0, b) + popped.charAt(b)
+							+ label.getText().substring(b + 1);
 					label.setText(newString);
+
 				}
+				counter++;
 			}
-		} 
-		if (popped.contains(""+character)) {
+
+		}
+		if (counter2 < size) {
+			System.out.println("Words Done "+counter2);
+		
+			System.out.println("Letters Guessed "+counter);
 			
+			System.out.println("Word Length "+popped.length());
+			
+			if (counter == popped.length()) {
+				popped = stack.pop();
+				int length = popped.length();
+				String popped2 = "";
+				for (int a = 0; a < length; a++) {
+					popped2 = popped2 + "_";
+				}
+				System.out.println(popped);
+				System.out.println(popped2);
+				label.setText(popped2);
+				LIVES = 6;
+				lives.setText("                            LIVES: " + LIVES);
+				counter2++;
+				counter = 0;
+			}
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
